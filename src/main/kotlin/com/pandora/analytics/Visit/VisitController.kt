695610fa16c7647952dc6ebb15ba5f63a41ea6cb
Valentin Kormanov <guve4e@gmail.com>
2019-09-27 22:22:53 -0400

    Adds Mongo Db credentials

--- src/main/kotlin/com/pandora/analytics/Visit/VisitController.kt
+++ src/main/kotlin/com/pandora/analytics/Visit/VisitController.kt
@@ -2,24 +2,21 @@ package com.pandora.analytics.Visit
 
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.toFlux
 
 @Controller
+@RequestMapping("/visits")
 class VisitController(private val visitRepository: VisitRepository) {
 
 //    @GetMapping("/visits")
 //    fun getAllVisits(): Flux<Visit> =
 //            visitRepository.findAll()
 
-    @PostMapping("/visits")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun makeAVisit(visit: Visit) =
+    fun makeAVisit(@RequestBody visit: Visit) =
             visitRepository.save(visit)
 
 }
-
-class HttpServletResponse(public val a: Integer, public val b: Integer)
--- src/test/kotlin/com/pandora/analytics/Visit/VisitControllerTest.kt
+++ src/test/kotlin/com/pandora/analytics/Visit/VisitControllerTest.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -35,5 +36,13 @@ internal class VisitControllerTest {
                 .exchange()
                 .expectStatus()
                 .isCreated
+
+        val a =  client
+                .get()
+                .uri("/visits")
+                .exchange()
+                .expectBody<Visit>()
+                .returnResult()
+                .responseBody
     }
 }
