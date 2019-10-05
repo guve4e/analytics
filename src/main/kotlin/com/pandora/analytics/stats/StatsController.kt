f55e72d5bf149f83f6bcb392c7e520e7f1d06bef
Valentin Kormanov <guve4e@gmail.com>
2019-10-05 18:05:10 -0400

    Adds get method for Stats object

--- src/main/kotlin/com/pandora/analytics/stats/StatsController.kt
+++ src/main/kotlin/com/pandora/analytics/stats/StatsController.kt
@@ -2,6 +2,7 @@ package com.pandora.analytics.stats
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import com.pandora.analytics.visit.Stats as Stats1
 
@@ -9,19 +10,22 @@ import com.pandora.analytics.visit.Stats as Stats1
 @RequestMapping("/stats")
 class StatsController(private val statsRepository: StatsRepository) {
 
+    @GetMapping
+    fun getStats(): Flux<Stats1> =
+            statsRepository.findAll()
+
     @PostMapping("/post")
-    fun updateProduct(): Mono<Stats1> {
-        return statsRepository.save(Stats1("1"))
-    }
+    fun makeStatObject(): Mono<Stats1> =
+            statsRepository.save(Stats1("1"))
 
     @PostMapping
-    fun updateProduct(@RequestBody visit: Visit): Mono<ResponseEntity<com.pandora.analytics.visit.Stats>> {
-        return statsRepository.findById("1")
+    fun addVisit(@RequestBody visit: Visit): Mono<ResponseEntity<Stats1>> =
+        statsRepository.findById("1")
                 .flatMap { s ->
                     s.visits.add(visit)
                     statsRepository.save(s)
                 }
                 .map { updateProduct -> ok(updateProduct) }
                 .defaultIfEmpty(ResponseEntity.notFound().build())
-    }
+
 }
